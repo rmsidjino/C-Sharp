68bde15c3dbe3281c478e286657d408d96b9895e
GDC8 <GDC8@DESKTOP-3J7DTMS>
2021-08-03 10:42:15 +0900

    초기 커밋.

---
+++ ConsoleApp0803/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0803
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //표준입출력 
+            string name = "홍길동";
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
